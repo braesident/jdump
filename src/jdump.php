f679e4de4b981d7e7f212705f6aa785ebcd7225f
braesident <braesie@hotmail.de>
2021-05-21 09:43:39 +0200

    functionality for numbers strings objects and arrays

--- src/jdump.php
+++ src/jdump.php
@@ -2,5 +2,22 @@
 
 function jdump($value)
 {
-	die(var_dump($value));
+	if(is_bool($value) || is_numeric($value))
+      die(var_dump($value));
+    
+    if (is_string($value)) {
+      try {
+        $d = json_decode($value);
+        if (is_null($d) && !is_null($value))
+          die(var_dump($value));
+        $j = json_encode($d, 512);
+        header('Content-Type: application/json');
+        die($j);
+      }
+      catch (Exception $e) {
+        die(var_dump($value));
+      }
+    }
+    header('Content-Type: application/json');
+    die(json_encode($value, 512));
 }
